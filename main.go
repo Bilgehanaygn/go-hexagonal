212a01b5f8d48fcdfc94086e110a94a35b1dafe6
bilgehan.aygun <bilgehan.aygun@tubitak.gov.tr>
2025-06-24 11:16:28 +0300

    fmt

--- main.go
+++ main.go
@@ -9,26 +9,26 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main(){
+func main() {
 	godotenv.Load()
 	port := os.Getenv("PORT")
 
-	if port == ""{
+	if port == "" {
 		log.Fatal("Port not found in the env fil")
 	}
 	fmt.Print("Port:", port)
 
-	  m, err := migrate.New(
-    "file://path/to/migrations",
-    "postgres://user:pass@localhost:5432/dbname?sslmode=disable",
-  )
+	m, err := migrate.New(
+		"file://path/to/migrations",
+		"postgres://user:pass@localhost:5432/dbname?sslmode=disable",
+	)
 
-    if err != nil {
-    log.Fatalf("failed to initialize migrations: %v", err)
-  }
+	if err != nil {
+		log.Fatalf("failed to initialize migrations: %v", err)
+	}
 
-  // this is what actually *runs* the migrations
-  if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-    log.Fatalf("migration failed: %v", err)
-  }
+	// this is what actually *runs* the migrations
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatalf("migration failed: %v", err)
+	}
 }
